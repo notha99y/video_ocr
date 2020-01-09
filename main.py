cd0dcd2b55c36e4d89e85fec099675f79ce16007
lianzy81 <lianzy@gmail.com>
2020-01-09 15:59:07 +0800

    updated main.py with crop coordinates

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def perform_ocr(crop):
     '''
     crop is a numpy array
     '''
-    ret, imgthresh = cv2.threshold(crop_img, 110, 255, cv2.THRESH_BINARY_INV)
+    # ret, imgthresh = cv2.threshold(crop, 110, 255, cv2.THRESH_BINARY_INV)
     # imgthresh2 = cv2.adaptiveThreshold(crop_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    text = pytesseract.image_to_string(imgthresh, lang="eng")
+    text = pytesseract.image_to_string(crop, lang="eng",
+                                       config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789')
 
     print('text: ', text)
     return text
@@ -37,14 +38,15 @@ if __name__ == "__main__":
 
     video_cap = cv2.VideoCapture(args.file_dir)
     get_fps(video_cap)
-
+    count = 0
     while (video_cap.isOpened()):
         ret, frame = video_cap.read()
         if ret == True:
-            cv2.imshow('Frame', frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-
+            air_speed_crop = frame[222:248,190:240]
+            timing_crop = frame[432:451, 63:200]
+            cv2.imwrite("frame_{}.png".format(count), air_speed_crop)
+            perform_ocr(air_speed_crop)
+            count += 1
         else:
             break
 
